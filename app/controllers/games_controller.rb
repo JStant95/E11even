03390b1c4b338cfcf881c1ea5e32f5896f37bb0c
Jed Stanton <jedstanton@hotmail.com>
2020-08-26 16:49:59 +0100

    methods added to game controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,43 @@
 class GamesController < ApplicationController
+  before_action :set_todo, only: [:show, :update, :destroy]
+
+    # GET /games
+    def index
+      @games = Game.all
+      json_response(@games)
+    end
+
+    # POST /games
+    def create
+      @game = Game.create!(game_params)
+      json_response(@game, :created)
+    end
+
+    # GET /games/:id
+    def show
+      json_response(@tgame)
+    end
+
+    # PUT /games/:id
+    def update
+      @todo.update(game_params)
+      head :no_content
+    end
+
+    # DELETE /games/:id
+    def destroy
+      @game.destroy
+      head :no_content
+    end
+
+    private
+
+    def game_params
+      params.permit(:draft_type)
+    end
+
+    def set_todo
+      @game = Game.find(params[:id])
+    end
+  end
 end
