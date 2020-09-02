6b5987d8038cf8a51e43e6a13f55563877e36350
Jed Stanton <jedstanton@hotmail.com>
2020-09-02 18:13:59 +0100

    Validation added to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :gamers
   has_many :games, through: :gamers
+   validates_presence_of :name, :username, :email, :password_digest
 end
