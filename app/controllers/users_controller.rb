507de50a832fc120d5f917ed6702e65be8f2cec0
Jed Stanton <jedstanton@hotmail.com>
2020-09-03 13:37:40 +0100

    Removed bcrypt functionality temporarily

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  include ActionController::MimeResponds
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.json { render json: @user, only: [:username, :created_at] }
+      else
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :username, :name, :password)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_secure_password
+  # has_secure_password
   has_many :gamers
   has_many :games, through: :gamers
-   validates_presence_of :name, :username, :email, :password_digest
+  validates_presence_of :name, :username, :email, :password
 end
