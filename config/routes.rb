d50be8948cef6cb8d6fabce36db6780382fb206b
Jed Stanton <jedstanton@hotmail.com>
2020-09-04 12:33:01 +0100

    new shallow routing setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :games do
-    resources :gamers
+  shallow do
+    resources :games do
+      resources :gamers
+      resources :pool
+        resources :player_pools
+    end
   end
 
   resources :users
+  resources :players
 end
