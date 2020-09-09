5cdd93c4348149aa9c3f9c60e41e1165593f682e
Jed Stanton <jedstanton@hotmail.com>
2020-09-09 16:30:07 +0100

    Player seed setup with playername, position, nationality and dob

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,16 @@
 require 'csv'
 
+i = 0
+
 CSV.foreach(Rails.root.join('lib/players_20.csv'), headers: true) do |row|
+  i += 1
 
   Player.create({
-    playername: row[2]
+    playername: row[2],
+    position: row[14],
+    nationality: row[8],
+    dob: row[5]
   })
+
+  break if i > 4800
 end
